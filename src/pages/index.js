8e626126b8bb1fa0f2b3d1a31aba1cb9312c9ad6
Othmane El Aissati <othmane.elaissati@student.ap.be>
2021-10-22 16:46:26 +0200

    plugins geregeld

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
-import * as React from "react"
-import Layout from "../components/layout"
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
-        <p>Lorem ipsum</p>
+      <p>Lorem ipsum</p>
+      <StaticImage
+        alt="randomized unsplash image!"
+        src="https://source.unsplash.com/random/800x600"
+      />
       </Layout>
     </main>
   )
